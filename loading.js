0ec5c1fd7777ee32a2e26ce34e3fdfa1a6795e60
Eng-frontend <114280645+Eng-frontend@users.noreply.github.com>
2023-07-06 14:37:29 +0300

    Add files via upload

---
+++ loading.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+location.href ="loading.html";
+};
+let contanier_test = document.getElementById("contanier_animation_load");
+let time = 500;
+window.onload = function(){
+    let Fun_Set = setInterval(function(){
+        contanier_test.style.display = "block";
+        time = time + 500;
+    if(time === 5000){
+        function stop(){
+            clearInterval(Fun_Set);
+            contanier_test.style.display = "none";
+            location.href ="index.html"; 
+        }
+        stop();
+    }
+},time); 
+}
